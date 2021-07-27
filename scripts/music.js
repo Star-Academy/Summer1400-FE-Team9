f7c9ccd2a1da0108c1f8bd5285cf61f56f51b483
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-27 22:44:46 +0430

    2x button functions completed

--- scripts/music.js
+++ scripts/music.js
@@ -1,12 +1,21 @@
 let music = document.getElementById("music");
-let twoXButton=document.getElementById("two-x-button");
+let twoXButton = document.getElementById("two-x-button");
 
 function setPlaybackRate(rate) {
     music.playbackRate = rate;
     if (rate === 2) {
-        twoXButton.style.borderWidth = "0";
-    } else {
         twoXButton.style.borderWidth = "0.35rem";
+    } else {
+        twoXButton.style.borderWidth = "0";
+    }
+}
+
+function changePlaybackRate() {
+    if (music.playbackRate === 2) {
+        setPlaybackRate(1);
+    }
+    else {
+        setPlaybackRate(2);
     }
 }
 
