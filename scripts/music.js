c2caff19a5c44ff9a9820ad100a9aff9205f4281
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-07-27 22:41:38 +0430

    2x button function added and styled

--- scripts/music.js
+++ scripts/music.js
@@ -1,6 +1,14 @@
 let music = document.getElementById("music");
+let twoXButton=document.getElementById("two-x-button");
 
-
+function setPlaybackRate(rate) {
+    music.playbackRate = rate;
+    if (rate === 2) {
+        twoXButton.style.borderWidth = "0";
+    } else {
+        twoXButton.style.borderWidth = "0.35rem";
+    }
+}
 
 document.getElementById("add-to-favorites-button").onclick = ev => {
     // TODO: Add to favorites
