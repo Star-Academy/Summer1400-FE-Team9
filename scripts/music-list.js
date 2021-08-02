618e6e4021300d01827676048388146a3a746470
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-02 14:46:42 +0430

    Changed JS file to be ready for server connection

--- scripts/music-list.js
+++ scripts/music-list.js
@@ -1,49 +1,51 @@
 // MARK: Fields and variables
 
+// let musics = []
+
 let musics = [
     {
-        title: "عنوان آهنگ ۱",
-        singer: "خواننده آهنگ ۱",
-        imageURL: "images/music-art.png",
-        audioURL: "assets/audios/music.mp3"
+        name: "عنوان آهنگ ۱",
+        artist: "خواننده آهنگ ۱",
+        cover: "images/music-art.png",
+        file: "assets/audios/music.mp3"
     },
     {
-        title: "عنوان آهنگ ۲",
-        singer: "خواننده آهنگ ۲",
-        imageURL: "images/music-art.png",
-        audioURL: "assets/audios/music.mp3"
+        name: "عنوان آهنگ ۲",
+        artist: "خواننده آهنگ ۲",
+        cover: "images/music-art.png",
+        file: "assets/audios/music.mp3"
     },
     {
-        title: "عنوان آهنگ ۳",
-        singer: "خواننده آهنگ ۳",
-        imageURL: "images/music-art.png",
-        audioURL: "assets/audios/music.mp3"
+        name: "عنوان آهنگ ۳",
+        artist: "خواننده آهنگ ۳",
+        cover: "images/music-art.png",
+        file: "assets/audios/music.mp3"
     },
     {
-        title: "عنوان آهنگ ۴",
-        singer: "خواننده آهنگ ۴",
-        imageURL: "images/music-art.png",
-        audioURL: "assets/audios/music.mp3"
+        name: "عنوان آهنگ ۴",
+        artist: "خواننده آهنگ ۴",
+        cover: "images/music-art.png",
+        file: "assets/audios/music.mp3"
     },
     {
-        title: "عنوان آهنگ ۵",
-        singer: "خواننده آهنگ ۵",
-        imageURL: "images/music-art.png",
-        audioURL: "assets/audios/music.mp3"
+        name: "عنوان آهنگ ۵",
+        artist: "خواننده آهنگ ۵",
+        cover: "images/music-art.png",
+        file: "assets/audios/music.mp3"
     },
     {
-        title: "عنوان آهنگ ۶",
-        singer: "خواننده آهنگ ۶",
-        imageURL: "images/music-art.png",
-        audioURL: "assets/audios/music.mp3"
+        name: "عنوان آهنگ ۶",
+        artist: "خواننده آهنگ ۶",
+        cover: "images/music-art.png",
+        file: "assets/audios/music.mp3"
     },
     {
-        title: "عنوان آهنگ ۷",
-        singer: "خواننده آهنگ ۷",
-        imageURL: "images/music-art.png",
-        audioURL: "assets/audios/music.mp3"
+        name: "عنوان آهنگ ۷",
+        artist: "خواننده آهنگ ۷",
+        cover: "images/music-art.png",
+        file: "assets/audios/music.mp3"
     }
-]
+];
 
 // MARK: UI Functions
 
@@ -63,7 +65,7 @@ function renderImage(music) {
     let img = document.createElement("img");
     img.setAttribute("height", "60");
     img.setAttribute("width", "60");
-    img.setAttribute("src", music.imageURL);
+    img.setAttribute("src", music.cover);
     img.setAttribute("alt", "عکس موزیک");
     return img;
 }
@@ -121,14 +123,14 @@ function renderTitleAndSingerLink(musicTitleAndSingerSpan) {
 function renderMusicTitle(music) {
     let musicTitleSpan = document.createElement("span");
     musicTitleSpan.setAttribute("class", "music-title");
-    musicTitleSpan.innerHTML = music.title;
+    musicTitleSpan.innerHTML = music.name;
     return musicTitleSpan;
 }
 
 function renderMusicSinger(music) {
     let musicSingerSpan = document.createElement("span");
     musicSingerSpan.setAttribute("class", "music-singer");
-    musicSingerSpan.innerHTML = music.singer;
+    musicSingerSpan.innerHTML = music.artist;
     return musicSingerSpan;
 }
 
@@ -180,3 +182,15 @@ function renderMusicList(musics) {
 }
 
 renderMusicList(musics);
+
+// async function loadAllMusics() {
+//     let response = await fetch('http://130.185.120.192:5000/song/all');
+//     if (response.ok) {
+//         musics = await response.json();
+//         renderMusicList(musics);
+//     } else {
+//         console.log("Server error");
+//     }
+// }
+//
+// loadAllMusics();
