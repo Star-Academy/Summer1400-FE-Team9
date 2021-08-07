bc474242dc9584a8f9c6eb46dc0d0fa246fb07c2
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-08-07 12:23:12 +0430

    theme toggle script of index page completed

--- scripts/index.js
+++ scripts/index.js
@@ -13,8 +13,120 @@ let checkbox = document.getElementById("toggle");
 
 checkbox.addEventListener('change', function() {
     if (this.checked) {
-        console.log("Checkbox is checked..");
+        setLight();
     } else {
-        console.log("Checkbox is not checked..");
+        setDark();
     }
 });
+
+// MARK: Light Theme
+
+primary= "#ff7e44";
+secondary= "#3a3f55";
+content_color= "wheat";
+text_color= "#000000";
+nav_color= "white";
+footer_color="white";
+title_color= "whitesmoke";
+
+// MARK: Dark Theme
+
+primary_dark= "#3a3f55";
+secondary_dark= "#ff7e44";
+content_color_dark= "lightgray";
+text_color_dark= "#000000";
+nav_color_dark= "white";
+footer_color_dark= "white";
+title_color_dark= "whitesmoke";
+
+function setLight(){
+    document.getElementsByTagName("style")[0].innerHTML=
+        "  body > header {\n" +
+        "    background-color: "+primary+";\n" +
+        "  }\n" +
+        "\n" +
+        "  #selected-tab, #main-selected-tab {\n" +
+        "    color: "+content_color+";\n" +
+        "  }\n" +
+        "\n" +
+        "  .more-buttons ul li:hover {\n" +
+        "    background-color: "+secondary+";\n" +
+        "  }\n" +
+        "\n" +
+        "  .hidden .hidden-text {\n" +
+        "    color: "+nav_color+";\n" +
+        "  }\n" +
+        "\n" +
+        "  body {\n" +
+        "    color: "+text_color+";\n" +
+        "    background-color: "+primary+";\n" +
+        "  }\n" +
+        "\n" +
+        "  .logo h1 {\n" +
+        "    color: "+content_color+";\n" +
+        "  }\n" +
+        "\n" +
+        "  header .site-title {\n" +
+        "    color: "+text_color+";\n" +
+        "  }\n" +
+        "  header ul li.primary-button {\n" +
+        "    background-color: "+secondary+";\n" +
+        "  }\n" +
+        "  header ul li a {\n" +
+        "    color: "+nav_color+";\n" +
+        "  }\n" +
+        "  header ul li:hover {\n" +
+        "    background-color: "+secondary+";\n" +
+        "  }\n" +
+        "\n" +
+        "  footer {\n" +
+        "    color: "+footer_color+";\n" +
+        "  }\n" +
+        "}\n";
+}
+
+function setDark(){
+    document.getElementsByTagName("style")[0].innerHTML=
+        "  body > header {\n" +
+        "    background-color: "+primary_dark+";\n" +
+        "  }\n" +
+        "\n" +
+        "  #selected-tab, #main-selected-tab {\n" +
+        "    color: "+content_color_dark+";\n" +
+        "  }\n" +
+        "\n" +
+        "  .more-buttons ul li:hover {\n" +
+        "    background-color: "+secondary_dark+";\n" +
+        "  }\n" +
+        "\n" +
+        "  .hidden .hidden-text {\n" +
+        "    color: "+nav_color_dark+";\n" +
+        "  }\n" +
+        "\n" +
+        "  body {\n" +
+        "    color: "+text_color_dark+";\n" +
+        "    background-color: "+primary_dark+";\n" +
+        "  }\n" +
+        "\n" +
+        "  .logo h1 {\n" +
+        "    color: "+content_color_dark+";\n" +
+        "  }\n" +
+        "\n" +
+        "  header .site-title {\n" +
+        "    color: "+text_color_dark+";\n" +
+        "  }\n" +
+        "  header ul li.primary-button {\n" +
+        "    background-color: "+secondary_dark+";\n" +
+        "  }\n" +
+        "  header ul li a {\n" +
+        "    color: "+nav_color_dark+";\n" +
+        "  }\n" +
+        "  header ul li:hover {\n" +
+        "    background-color: "+secondary_dark+";\n" +
+        "  }\n" +
+        "\n" +
+        "  footer {\n" +
+        "    color: "+footer_color_dark+";\n" +
+        "  }\n" +
+        "}\n";
+}
