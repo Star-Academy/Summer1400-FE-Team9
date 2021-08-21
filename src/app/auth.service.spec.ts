ac9666ad939631d135cf0f56a5670516e1fda71d
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-21 15:45:47 +0430

    Tests for home view, auth service and guard service

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -2,16 +2,50 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
 
+import 'jasmine-ajax';
+
 describe('AuthService', () => {
   let service: AuthService;
+  let originalFetch: any;
 
   beforeEach(() => {
     TestBed.configureTestingModule({providers: [AuthService]});
     TestBed.configureTestingModule({});
     service = TestBed.inject(AuthService);
+
+    //   originalFetch = (window as any).fetch;
+    //   (window as any).fetch = fetchPolyfill;
+    //   jasmine.Ajax.install();
+    //
+    //   jasmine.Ajax.stubRequest(/.*\/user\/login/).andError({
+    //     status: 200,
+    //     statusText: 'testToken'
+    //   });
+    //
+    //   jasmine.Ajax.stubRequest(/.*\/playlist\/create/).andError({
+    //     status: 200,
+    //     statusText: "{ 'status': 'success' }"
+    //   });
+    //
+    //   jasmine.Ajax.stubRequest(/.*\/user\/register/).andError({
+    //     status: 200,
+    //     statusText: "{ 'status': 'success' }"
+    //   });
+    // });
+    //
+    // afterEach(() => {
+    //   jasmine.Ajax.uninstall();
+    //   (window as any).fetch = originalFetch;
+    // });
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it("should log out", () => {
+    localStorage.setItem("is-logged-in", "true");
+    service.logout();
+    expect(localStorage.getItem("is-logged-in")).toEqual("false");
+  })
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,20 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
-import { Router } from "@angular/router";
+import {HomeComponent} from './home.component';
+import {Router} from "@angular/router";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  let hasNavigated = false;
+  let navigatedString = "";
+
   let router = {
-    navigateByUrl: () => { }
+    navigateByUrl: (destination: string) => {
+      hasNavigated = true;
+      navigatedString = destination;
+    }
   };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
-      providers: [{ provide: Router, useValue: router }]
+      providers: [{provide: Router, useValue: router}]
     })
       .compileComponents();
   });
@@ -27,5 +33,26 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the correct local storage item', () => {
+    localStorage.setItem("local-storage-test", "test-value");
+    expect(component.localStorageItem("local-storage-test")).toEqual("test-value");
+  });
+
+  it('should primary button callback work', async () => {
+    localStorage.setItem("is-logged-in", "true");
+    await component.primaryButtonTapped();
+    expect(hasNavigated).toBeTruthy();
+    expect(navigatedString).toEqual("music-list-page");
+    hasNavigated = false;
+    navigatedString = "";
+
+    localStorage.setItem("is-logged-in", "false");
+    await component.primaryButtonTapped();
+    expect(hasNavigated).toBeTruthy();
+    expect(navigatedString).toEqual("../register-form");
+    hasNavigated = false;
+    navigatedString = "";
   });
 });
