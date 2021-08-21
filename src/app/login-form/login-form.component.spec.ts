032ea9dccac4d2a556c8f9ac888ab4dba18f15ca
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-21 13:49:06 +0430

    Fixed login-form.component.spec.ts tests + changes in the API

--- src/app/login-form/login-form.component.spec.ts
+++ src/app/login-form/login-form.component.spec.ts
@@ -2,21 +2,34 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginFormComponent } from './login-form.component';
 import {AuthService} from "../auth.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {MusicLoaderService} from "../music-loader.service";
+import {Subject} from "rxjs";
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
 
+  let router = {
+    navigateByUrl: (url: string) => Promise.resolve(true)
+  };
+
+  let authService = {
+    login: (email: string, password: string) => Promise.resolve("")
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
+      declarations: [ LoginFormComponent ],
+      providers: [
+        { provide: Router, useValue: router },
+        { provide: AuthService, useValue: authService }
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [AuthService, Router, Function]});
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
