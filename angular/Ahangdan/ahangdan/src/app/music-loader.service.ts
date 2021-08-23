3235595a8e4bbe3e4f3f8ff831cd0ae6aaabdf2a
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-23 15:37:38 +0430

    Added HttpClient support to MusicLoaderService

--- angular/Ahangdan/ahangdan/src/app/music-loader.service.ts
+++ angular/Ahangdan/ahangdan/src/app/music-loader.service.ts
@@ -1,20 +1,26 @@
 import {Injectable} from '@angular/core';
 import Music from "./models/MusicModel";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class MusicLoaderService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
-  public static async sendRequest(url: string, method: string, body?: object): Promise<any> {
-    const init: RequestInit = {headers: {'Content-Type': 'application/json'}};
-    init.method = method;
-    if (body) init.body = JSON.stringify(body);
-    return fetch(url, init).then((response) => {
-      if (response.ok) return response.json();
-      throw response.text();
-    })
+  // public static async sendRequest(url: string, method: string, body?: object): Promise<any> {
+  //   const init: RequestInit = {headers: {'Content-Type': 'application/json'}};
+  //   init.method = method;
+  //   if (body) init.body = JSON.stringify(body);
+  //   return fetch(url, init).then((response) => {
+  //     if (response.ok) return response.json();
+  //     throw response.text();
+  //   })
+  // }
+
+  public async sendRequest(url: string, method: string, body?: object): Promise<any> {
+    if (method == 'POST') return this.http.post<any>(url, body, {headers: {'Content-Type': 'application/json'}}).toPromise();
+    else return this.http.get<any>(url, {headers: {'Content-Type': 'application/json'}}).toPromise();
   }
 
   public static async sendNonJSONRequest(url: string, method: string, body?: object): Promise<any> {
@@ -28,9 +34,9 @@ export class MusicLoaderService {
   }
 
   async getAllMusics(): Promise<Music[]> {
-    const result = await MusicLoaderService.sendRequest("https://songs.code-star.ir/song/all", 'GET');
+    const result = await this.sendRequest("https://songs.code-star.ir/song/all", 'GET');
     const musics = result.songs;
-    const playlistResult = await MusicLoaderService.sendRequest("https://songs.code-star.ir/playlist/all",
+    const playlistResult = await this.sendRequest("https://songs.code-star.ir/playlist/all",
       'POST',
       {token: localStorage.getItem("token")});
     let finalMusics = musics.map((music: any) => new Music(music));
