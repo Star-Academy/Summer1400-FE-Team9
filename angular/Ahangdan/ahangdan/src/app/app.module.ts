469c233b357aeefa2c4ab166349fea9a6ec883a5
Seyyed Parsa Neshaei <seyyedparsa@outlook.com>
2021-08-24 15:57:48 +0430

    Used material icons for musics page

--- angular/Ahangdan/ahangdan/src/app/app.module.ts
+++ angular/Ahangdan/ahangdan/src/app/app.module.ts
@@ -1,29 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterbarComponent } from './footerbar/footerbar.component';
-import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { MusicListPageComponent } from './music-list-page/music-list-page.component';
-import { MusicPageComponent } from './music-page/music-page.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterbarComponent} from './footerbar/footerbar.component';
+import {HomeComponent} from './home/home.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {LoginFormComponent} from './login-form/login-form.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
+import {MusicListPageComponent} from './music-list-page/music-list-page.component';
+import {MusicPageComponent} from './music-page/music-page.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {AuthService} from "./auth.service";
 import {MusicLoaderService} from "./music-loader.service";
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import {GuardService} from "./guard.service";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
-import { HttpClientModule } from '@angular/common/http';
-import { TextboxComponent } from './textbox/textbox.component';
-import { NgxAudioPlayerModule } from 'ngx-audio-player';
+import {HttpClientModule} from '@angular/common/http';
+import {TextboxComponent} from './textbox/textbox.component';
+import {NgxAudioPlayerModule} from 'ngx-audio-player';
+import {MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import { NgxAudioPlayerModule } from 'ngx-audio-player';
     MatInputModule,
     MatButtonModule,
     HttpClientModule,
-    NgxAudioPlayerModule
+    NgxAudioPlayerModule,
+    MatIconModule
   ],
   providers: [
     AuthService,
@@ -58,4 +60,5 @@ import { NgxAudioPlayerModule } from 'ngx-audio-player';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
